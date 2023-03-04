db254847151a96cb7277921c1680b567b794b516
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-04 09:18:00 -0800

    Add files via upload

---
+++ task4.2.py
@@ -0,0 +1,22 @@
+
+#task2
+a=input('enter your name:')
+b=input('enter your qualification:')
+c=input('enter the course name you are interested:')
+d=int(input('enter your whatspp number:'))
+print('name:',a,'qualification:',b,'course name:','whatsapp number:',d)
+#task3
+#reg form for python agency
+a=input('enter your name:')
+b=input('enter email ID:')
+d=input('enter your confirm password:')
+e=input('enter username:')
+print('name:',a,'Email ID:',b,'password:',c,'confirm password:',d,'username',e)
+#Log in
+
+a=input('enter your usernname:')
+b=input('enter your password:')
+print('username:',a,'password:',b)
+
+
+
